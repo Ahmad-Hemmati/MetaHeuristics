c084a4b1ce6a620a902269765fccf416c0cf995f
AnandhuDileep <62464377+AnandhuDileep@users.noreply.github.com>
2026-02-12 14:44:59 +0100

    Update coords_to_instance_converter.py

--- Truck-and-drones challenge/coords_to_instance_converter.py
+++ Truck-and-drones challenge/coords_to_instance_converter.py
@@ -3,14 +3,12 @@ import math
 import numpy as np
 
 
-filenames = "F10.txt"
+filename = "F10.txt"
 
-flight_range = [4600]
+flight_range = 4600
 
-filename = filenames[i]
 n_drones = 2 #instance file
 speed_ratio = 1.5 # instance file
-flight_range = flight_ranges[i]
 drone_capacity = 1 # instance file
 depot_index=0 # instance file
 
@@ -51,68 +49,68 @@ def parse_solution(values: str):
     
     
     
-    def read_coordinates(filename: str):
-        """
-        Read coordinates from a file formatted like TRP-S50-R1.txt,
-        where each line contains two integers:  X<TAB>Y
-    
-        Returns:
-            List[Tuple[int, int]]
-        """
-        coords = []
-        with open(filename, "r") as f:
-            for line in f:
-                line = line.strip()
-                if not line:
-                    continue  # skip empty lines
-                
-                parts = line.split()
-                if len(parts) != 2:
-                    raise ValueError(f"Invalid line (expected 2 numbers): {line}")
-    
-                x, y = map(int, parts)
-                coords.append((x, y))
-        print(coords)
-        return coords
-    
-    
-    def create_target_solution_instance():
-        """Create problem instance that naturally leads to the target solution"""
-        coords = read_coordinates(filename)
-        n_nodes = len(coords) -1 # first and last node is the depot
-        n_customers = n_nodes -1
-        distance_matrix = np.zeros((n_nodes, n_nodes))
-    
-        for i in range(n_nodes):
-            for j in range(n_nodes):
-                dx = coords[i][0] - coords[j][0]
-                dy = coords[i][1] - coords[j][1]
-                distance_matrix[i][j] = math.sqrt(dx**2 + dy**2)
-    
-        # print("Euclidean Distance Matrix (11 nodes):")
-        # print(np.array2string(distance_matrix, formatter={'float_kind': lambda x: "%.2f" % x}))
-    
-        T = np.floor(distance_matrix)  #truck_times
-        drone_times=np.floor(T / speed_ratio) *100
-        truck_times=T*100
-    
-        return n_nodes, n_customers, n_drones, truck_times, drone_times, flight_range,  drone_capacity
+def read_coordinates(filename: str):
+    """
+    Read coordinates from a file formatted like TRP-S50-R1.txt,
+    where each line contains two integers:  X<TAB>Y
+
+    Returns:
+        List[Tuple[int, int]]
+    """
+    coords = []
+    with open(filename, "r") as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue  # skip empty lines
+            
+            parts = line.split()
+            if len(parts) != 2:
+                raise ValueError(f"Invalid line (expected 2 numbers): {line}")
+
+            x, y = map(int, parts)
+            coords.append((x, y))
+    print(coords)
+    return coords
     
     
-    n_nodes, n_customers, n_drones, truck_times, drone_times, flight_range, drone_capacity = create_target_solution_instance()    
+def create_target_solution_instance():
+    """Create problem instance that naturally leads to the target solution"""
+    coords = read_coordinates(filename)
+    n_nodes = len(coords) -1 # first and last node is the depot
+    n_customers = n_nodes -1
+    distance_matrix = np.zeros((n_nodes, n_nodes))
+
+    for i in range(n_nodes):
+        for j in range(n_nodes):
+            dx = coords[i][0] - coords[j][0]
+            dy = coords[i][1] - coords[j][1]
+            distance_matrix[i][j] = math.sqrt(dx**2 + dy**2)
+
+    # print("Euclidean Distance Matrix (11 nodes):")
+    # print(np.array2string(distance_matrix, formatter={'float_kind': lambda x: "%.2f" % x}))
+
+    T = np.floor(distance_matrix)  #truck_times
+    drone_times=np.floor(T / speed_ratio) *100
+    truck_times=T*100
+
+    return n_nodes, n_customers, n_drones, truck_times, drone_times, flight_range,  drone_capacity
     
-    #"""
-    with open(filename[:-4]+"_.txt", "w") as f:
-        f.write("# Number of customers\n")
-        f.write(str(n_nodes-1)+"\n")
-        f.write("# Drone flight limit\n")
-        f.write(str(flight_range)+"\n")    
-        f.write("# Travel time matrix for the truck\n")
-        for row in truck_times:
-            f.write("\t".join(map(str, row)) + "\n")
-        f.write("# Travel time matrix for the drones\n")
-        for row in drone_times:
-            f.write("\t".join(map(str, row)) + "\n")
-        f.write("#\n")
-    #"""
+    
+n_nodes, n_customers, n_drones, truck_times, drone_times, flight_range, drone_capacity = create_target_solution_instance()    
+
+#"""
+with open(filename[:-4]+"_.txt", "w") as f:
+    f.write("# Number of customers\n")
+    f.write(str(n_nodes-1)+"\n")
+    f.write("# Drone flight limit\n")
+    f.write(str(flight_range)+"\n")    
+    f.write("# Travel time matrix for the truck\n")
+    for row in truck_times:
+        f.write("\t".join(map(str, row)) + "\n")
+    f.write("# Travel time matrix for the drones\n")
+    for row in drone_times:
+        f.write("\t".join(map(str, row)) + "\n")
+    f.write("#\n")
+#"""
     
