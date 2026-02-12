4f967790d8529c46da6bb262cc7150649ee9b3dc
AnandhuDileep <62464377+AnandhuDileep@users.noreply.github.com>
2026-02-12 14:29:40 +0100

    Add files via upload

--- Truck-and-drones challenge/main.py
+++ Truck-and-drones challenge/main.py
@@ -1,174 +1,122 @@
-from FeasibiltyCheck import SolutionFeasibility
-from CalCulateTotalArrivalTime import CalCulateTotalArrivalTime
+
+import math
 import numpy as np
 
 
-# -------------------------------------------
-filename = "toy_example.txt"
-n_drones = 2 #fixed
-drone_capacity = 1 #fixed
-depot_index=0 #fixed
+filenames = ["F10.txt"]
 
-example_solution_str = ("0,0,|,-1,|,-1,|,-1")  
-# ---------------------------------------------------------------------------
-def parse_solution(values: str):
-    """
-    values: string like "1,2,3,|,10,20,|,5,6,|,100,200"
-            or "1,2,3|10,20|5,6|100,200"
-    returns: {"part1": [...], "part2": [...], "part3": [...], "part4": [...]}
-    """
+flight_ranges = [4600]
 
-    if not isinstance(values, str):
-        raise TypeError(f"Expected string from Go, got {type(values)}")
+#filenames=[filenames[0]]
 
-    s = values.strip()
-    chars = list(s.split(",")) 
-    if chars.count('|') != 3:
-        raise ValueError(f"Expected 3 chars '|', got {chars.count('|')}. chars: {chars}")
+for i in range(len(filenames)):
 
-
-    # normalize around pipes: ",|," or ",|" or "|," -> "|"
-    #s = s.replace(",|,", "|").replace(",|", "|").replace("|,", "|")
-
-    parts = s.split("|")
-    if len(parts) != 4:
-        raise ValueError(f"Expected 4 parts separated by '|', got {len(parts)} parts: {parts}")
-
-    def parse_int_list(part: str):
-        # split by comma, ignore empty chunks (can happen after normalization)
-        chunks = [c.strip() for c in part.split(",") if c.strip() != ""]
-        return [int(c) for c in chunks]
-
-    return {
-        "part1": parse_int_list(parts[0]),
-        "part2": parse_int_list(parts[1]),
-        "part3": parse_int_list(parts[2]),
-        "part4": parse_int_list(parts[3]),
-    }
-
-
-
-def read_data(filename: str):
-
-    truck_times = []
-    drone_times = []
-    hash_count=0
-    with open(filename, "r") as f:
-        for line in f:
-            line = line.strip()
-            if line[0]!="#":
-               row = list(map(float, line.strip().split()))
-               if hash_count == 1:
-                  n_customers = int(row[0])
-               if hash_count == 2:
-                  flight_range = int(row[0])
-               elif hash_count == 3:
-                  truck_times.append(row)
-               elif hash_count == 4:
-                  drone_times.append(row) 
-            else:
-               hash_count+=1
-            
-    n_nodes = n_customers+1 
-    truck_times=np.array(truck_times)
-    drone_times=np.array(drone_times)
-
-    return n_nodes, n_customers, n_drones, flight_range, truck_times, drone_times, flight_range,  drone_capacity
-
-
-n_nodes, n_customers, n_drones, flight_range, truck_times, drone_times, flight_range, drone_capacity = read_data(filename)
-
-# -------------------------------------------
-
-class SolutionRunner(CalCulateTotalArrivalTime, SolutionFeasibility):
-    def __init__(
-        self,
-        solution: dict,
-        truck_times,
-        flight_time_matrix,
-        flight_range_limit: float,
-        depot_index: int = depot_index,
-        max_iterations: int = 10,
-        convergence_threshold: float = 1.0,
-        n_drones: int = n_drones,
-    ):
+    filename = filenames[i]
+    n_drones = 2 #instance file
+    speed_ratio = 1.5 # instance file
+    flight_range = flight_ranges[i]
+    drone_capacity = 1 # instance file
+    depot_index=0 # instance file
+    
+    example_solution_str = ("0,0,|,-1,|,-1,|,-1")
+     
+    # Problem instance data  
+    # ---------------------------------------------------------------------------
+    def parse_solution(values: str):
         """
-        Wraps feasibility check + total cost calculation in one object.
+        values: string like "1,2,3,|,10,20,|,5,6,|,100,200"
+                or "1,2,3|10,20|5,6|100,200"
+        returns: {"part1": [...], "part2": [...], "part3": [...], "part4": [...]}
         """
-        self.solution = solution
-        self.truck_times = truck_times
-        self.drone_times = drone_times
-        self.flight_time_matrix = flight_time_matrix
-        self.flight_range  = flight_range 
-        self.depot_index = depot_index
-        self.max_iterations = max_iterations
-        self.convergence_threshold = convergence_threshold
-        self.n_drones = n_drones
-
-        n_nodes = truck_times.shape[0]
-
-        # Create feasibility checker based on the instance
-        self.feasibility = SolutionFeasibility(
-            n_nodes=n_nodes,
-            n_drones=n_drones,
-            depot_index=depot_index,
-            drone_times=flight_time_matrix,
-            flight_range=flight_range_limit,
-        )
-
-    def run(self):
+    
+        if not isinstance(values, str):
+            raise TypeError(f"Expected string from Go, got {type(values)}")
+    
+        s = values.strip()
+    
+        # normalize around pipes: ",|," or ",|" or "|," -> "|"
+        s = s.replace(",|,", "|").replace(",|", "|").replace("|,", "|")
+    
+        parts = s.split("|")
+        if len(parts) != 4:
+            raise ValueError(f"Expected 4 parts separated by '|', got {len(parts)} parts: {parts}")
+    
+        def parse_int_list(part: str):
+            # split by comma, ignore empty chunks (can happen after normalization)
+            chunks = [c.strip() for c in part.split(",") if c.strip() != ""]
+            return [int(c) for c in chunks]
+    
+        return {
+            "part1": parse_int_list(parts[0]),
+            "part2": parse_int_list(parts[1]),
+            "part3": parse_int_list(parts[2]),
+            "part4": parse_int_list(parts[3]),
+        }
+    
+    
+    
+    def read_coordinates(filename: str):
         """
-        1) Check feasibility
-        2) If infeasible: print message and return None
-        3) If feasible: compute total waiting/arrival time and return it
+        Read coordinates from a file formatted like TRP-S50-R1.txt,
+        where each line contains two integers:  X<TAB>Y
+    
+        Returns:
+            List[Tuple[int, int]]
         """
-        sol = self.solution
-        f = self.feasibility
-
-        #print("=== FEASIBILITY CHECK ===")
-        truck_ok = f.is_truck_route_feasible(sol)
-        complete_ok = f.is_complete_solution(sol)
-        parts_ok = f.are_parts_consistent(sol)
-        drones_ok = f.are_all_drone_trips_feasible(sol)
-        global_ok = f.is_solution_feasible(sol)
-
-        print("Truck feasible   :", truck_ok)
-        print("Complete         :", complete_ok)
-        print("Parts consistent :", parts_ok)
-        print("Drone trips OK   :", drones_ok)
-
-        if not global_ok:
-            #print("\nCannot calculate the total cost, since the solution is not feasible.")
-            print("GLOBAL FEASIBLE  :", global_ok)
-            return {'error': '', 'feasible': False, 'objective': 0.0} 
-
-        # If we get here, the solution is feasible → compute total waiting time
-        total, arr, dep, feas = self.calculate_total_waiting_time(
-            solution=sol
-        )
-        
-        if not feas:
-           print("GLOBAL FEASIBLE  :", feas) 
-           #print("Flight range limits on one of the drones is not satisfied") 
-           return {'error': '', 'feasible': False, 'objective': 0.0}  
-
-        print("GLOBAL FEASIBLE  :", feas) 
-        print("Total objective:", float(total))
-        return {'error': '', 'feasible': True, 'objective': total}#total, arr, dep
-
-
-# get solution from input arguments
-solution = parse_solution(example_solution_str)
-
-runner = SolutionRunner(
-    solution=solution,
-    truck_times=truck_times,
-    flight_time_matrix=drone_times,
-    flight_range_limit=flight_range,
-    depot_index=depot_index,
-    max_iterations=10,
-    convergence_threshold=1.0,
-    n_drones=n_drones,
-)
-
-result = runner.run()  # prints feasibility and total arrival time if feasible
+        coords = []
+        with open(filename, "r") as f:
+            for line in f:
+                line = line.strip()
+                if not line:
+                    continue  # skip empty lines
+                
+                parts = line.split()
+                if len(parts) != 2:
+                    raise ValueError(f"Invalid line (expected 2 numbers): {line}")
+    
+                x, y = map(int, parts)
+                coords.append((x, y))
+        print(coords)
+        return coords
+    
+    
+    def create_target_solution_instance():
+        """Create problem instance that naturally leads to the target solution"""
+        coords = read_coordinates(filename)
+        n_nodes = len(coords) -1 # first and last node is the depot
+        n_customers = n_nodes -1
+        distance_matrix = np.zeros((n_nodes, n_nodes))
+    
+        for i in range(n_nodes):
+            for j in range(n_nodes):
+                dx = coords[i][0] - coords[j][0]
+                dy = coords[i][1] - coords[j][1]
+                distance_matrix[i][j] = math.sqrt(dx**2 + dy**2)
+    
+        # print("Euclidean Distance Matrix (11 nodes):")
+        # print(np.array2string(distance_matrix, formatter={'float_kind': lambda x: "%.2f" % x}))
+    
+        T = np.floor(distance_matrix)  #truck_times
+        drone_times=np.floor(T / speed_ratio) *100
+        truck_times=T*100
+    
+        return n_nodes, n_customers, n_drones, truck_times, drone_times, flight_range,  drone_capacity
+    
+    
+    n_nodes, n_customers, n_drones, truck_times, drone_times, flight_range, drone_capacity = create_target_solution_instance()    
+    
+    #"""
+    with open(filename[:-4]+"_.txt", "w") as f:
+        f.write("# Number of customers\n")
+        f.write(str(n_nodes-1)+"\n")
+        f.write("# Drone flight limit\n")
+        f.write(str(flight_range)+"\n")    
+        f.write("# Travel time matrix for the truck\n")
+        for row in truck_times:
+            f.write("\t".join(map(str, row)) + "\n")
+        f.write("# Travel time matrix for the drones\n")
+        for row in drone_times:
+            f.write("\t".join(map(str, row)) + "\n")
+        f.write("#\n")
+    #"""
+    
